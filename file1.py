406cdc9805fc40dbee536d8a7e1c3bc230fd487b
sheshank <sheshank1436@gmail.com>
2023-02-20 20:37:12 +0530

    rebase2 from master
    an example of rebase

--- file1.py
+++ file1.py
@@ -4,12 +4,7 @@ x=hello('mass maharaj')
 print(x)
 y=hello('rajinikanth')
 def world():
-<<<<<<< HEAD
-    return 'this is my world and you wont understand my rules'
-=======
-    return 'this is my world and my rules"
->>>>>>> b3cb237 (rebase2 from demo5)
-
+    return "enough"
 print(world())
 print(hello('salaam rocky bhai')
 print('edit from branch demo4')
