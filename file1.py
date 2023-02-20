aa8960c2b00d3062cafb1fd244db678c627c7fe0
sheshank <sheshank1436@gmail.com>
2023-02-20 20:22:02 +0530

    changes to file1.py from master
    i am changing only the commit message

--- file1.py
+++ file1.py
@@ -4,7 +4,14 @@ x=hello('mass maharaj')
 print(x)
 y=hello('rajinikanth')
 def world():
+<<<<<<< HEAD
     return 'this is my world and you wont understand my rules'
+
 print(world())
 print(hello('rocky bhai')
 print('edit from branch demo4')
+=======
+    return "enough"
+print(hello('salaam rocky bhai')
+print('edit from branch master')
+>>>>>>> b52805b (i am changing only the commit message yes)
